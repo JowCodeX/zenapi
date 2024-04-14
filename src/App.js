29cfa5e8113cb9dfb313c4ba5b6d6f08904fe279
Yusupha Jow <Jooow_@live.se>
2024-04-14 21:31:41 +0200

    Ready to Ruuuumbleeee

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import axios from 'axios';
+import './Quote.css';
+import { useEffect, useState } from 'react';
+
+const App = () => {
+  const [quote, setQuote] = useState("");  
+  const [author, setAuthor] = useState(""); 
+
+  const zenApiQuote = async () => {
+    
+    let dataFromQuotes = [];
+
+    try{
+      console.log('Request initiated to: /api/random'); 
+      const data = await axios.get("/api/random"); 
+      console.log('Response from ZenQuotes:', data); 
+      dataFromQuotes = data.data[0];
+    } catch (error) {
+      console.log(error);
+    }
+
+    try {
+      setQuote(dataFromQuotes.q);  
+      setAuthor(dataFromQuotes.a);   
+    }catch(error) {
+      console.log(error) ;
+    }
+  };
+  
+  useEffect(() => {
+    zenApiQuote(); 
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='boxForQuotes'>
+        <div className='container'>
+            <div className='newQuoteButton'>
+              {" "}
+              <button onClick={zenApiQuote}>New Quote</button>
+            </div>
+            <div className='quote'>
+              <h2>{quote}</h2>
+              </div> 
+            <div className='author'>{author}</div> 
+        </div>
+      </div>
+
     </div>
   );
-}
+};
 
 export default App;
